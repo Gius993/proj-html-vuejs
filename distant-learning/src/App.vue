ad8d5adee740a4195ab3f3b94167622a08dd8764
Gius993 <giuseppevidili9@gmail.com>
2022-07-15 12:17:53 +0200

    main collegato

--- distant-learning/src/App.vue
+++ distant-learning/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app"> 
     <AppHeader />
+    <AppMain />
   </div>
 </template>
 
 <script>
 
-import AppHeader from './components/AppHeader.vue'
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 
 export default {
   name: 'App',
   components: {
-    AppHeader
+    AppHeader,
+    AppMain
   }
 }
 </script>
